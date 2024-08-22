daa8b94c738ebe5fed78fe484183fbf73f41359a
port19 <port19@port19.xyz>
2024-08-22 16:51:59 +0200

    Heavy refactor that doesn't solve any of the async issues

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ const helptext = "Usage: deadsniper <link to sitemap.xml>"
 var exitCode int = 0
 var mutex sync.Mutex
 
+// This is a low level wrapper for http get requests to return the request body as a string or error otherwise.
 func reqWrap(url string) string {
 	resp, err := http.Get(url)
 	if err != nil {
@@ -24,24 +25,11 @@ func reqWrap(url string) string {
 	if err != nil {
 		log.Fatalf("reqWrap: failed to read response body: %v", err)
 	}
-	body2 := string(body)
-	return body2
+	return string(body)
 }
 
-func linkSlurp(url string) []string {
-	body := reqWrap(url)
-	re := regexp.MustCompile(`"(https://.+?)"`)
-	matches := re.FindAllStringSubmatch(body, -1)
-
-	var links []string
-	for _, matchSlice := range matches {
-		if len(matchSlice) > 1 {
-			links = append(links, matchSlice[1]) // append the first submatch
-		}
-	}
-	return links
-}
-
+// This checks a list of links. It also expects the corresponding toplevel, to make fixing easier
+// This is an outside function to make it easily parallelizable
 func isLinkAlive(url string, toplevel string) string {
 	resp, err := http.Get(url)
 	if err != nil || resp.StatusCode != http.StatusOK {
@@ -62,24 +50,34 @@ func main() {
 		return
 	}
 	url := os.Args[1]
-	body2 := reqWrap(url)
+	body := reqWrap(url)
 
+	// Parse all the requests from the sitemap file
 	re := regexp.MustCompile(`<loc>(https://.+?)</loc>`)
-	to_test := re.FindAllStringSubmatch(body2, -1)
-	var body3 []string
+	to_test := re.FindAllStringSubmatch(body, -1)
+	var sites_in_sitemap []string
 	for _, matchSlice := range to_test {
 		if len(matchSlice) > 1 {
-			body3 = append(body3, matchSlice[1])
+			sites_in_sitemap = append(sites_in_sitemap, matchSlice[1])
 		}
 	}
 
+	// Prepare waitgroup and channel for async processing
 	results := make(chan string)
 	var wg sync.WaitGroup
 
-	// Further parallelizing linkSlurp does not help due to CPU and Network Limitations, I tried
-	for _, toplevel := range body3 {
-		labrat := linkSlurp(toplevel)
-		for _, link := range labrat {
+	for _, toplevel := range sites_in_sitemap {
+		// This takes a toplevel site and populates a list with the links on that site
+		// Parallelizing this toplevel processing does not help due to CPU and Network Limitations, I tried
+		body := reqWrap(toplevel)
+		re := regexp.MustCompile(`"(https://.+?)"`)
+		matches := re.FindAllStringSubmatch(body, -1)
+		var links []string
+		for _, matchSlice := range matches {
+		  links = append(links, matchSlice[1]) // append the first submatch
+		}
+
+		for _, link := range links {
 			wg.Add(1)
 			go func(link string) {
 				defer wg.Done()
